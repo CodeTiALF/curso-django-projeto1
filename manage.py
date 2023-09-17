3d32103d1f6c17c7ac14281e51f717ecf34ed742
CodeTiALF <andre@andrefilus.com.br>
2023-09-17 20:09:51 -0300

    ok

--- manage.py
+++ manage.py
@@ -1,22 +1,16 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
- 
+class Usuario:
+    def __init__(self, nome, senha):
+        self.nome = nome
+        self.senha = senha
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+    def listar_atributos(self):
+        atributos = vars(self)  # Obtém um dicionário dos atributos da instância
+        for atributo, valor in atributos.items():
+            print(f'{atributo}: {valor}')
 
+# Criar uma instância da classe Usuario
+usuario1 = Usuario("joao123", "senha123")
 
-if __name__ == '__main__':
-    main()
+# Listar os atributos e valores do usuário
+print("Atributos e Valores do Usuário:")
+usuario1.listar_atributos()
